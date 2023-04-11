3f4402119497001b9a95963de0a6f1595f35389b
bossa96demo <ilyaershov09@gmail.com>
2023-04-11 16:39:01 +0300

    added more interaction

--- color.py
+++ color.py
@@ -1,6 +1,8 @@
 def main():
+    print("please, be sure that black and white are written after other colors")
     active = input("active hex (2 hex decimals): ")
     inactive = input("inactive hex (2 hex decimals): ")
+    name = input("your username: ")
     red =     "#" +   active + inactive + inactive
     green =   "#" + inactive +   active + inactive
     blue =    "#" + inactive + inactive +   active
@@ -13,14 +15,21 @@ def main():
     print("    yellow:  '" + yellow +  "'")
     print("    magenta: '" + magenta + "'")
     print("    cyan:    '" + cyan +    "'")
-    with open("/home/ilya/.config/alacritty/colors.yml", "r") as f:
+    with open("/home/" + name + "/.config/alacritty/colors.yml", "r") as f:
         lines = f.readlines()
-
-    with open("/home/ilya/.config/alacritty/colors.yml", "w") as f:
+    norb = input("do you want to paint normal or bright colors?(n or b): ")
+    if norb == "n":
+        check = "  normal:"
+    elif norb == "b":
+        check = "  bright:"
+    else:
+        print("what?")
+        check = "  normal:"
+    with open("/home/" + name + "/.config/alacritty/colors.yml", "w") as f:
         i = 0
         while i < len(lines):
-            if "  normal:" in lines[i]:
-                f.write("  normal:\n")
+            if check in lines[i]:
+                f.write(check + "\n")
                 f.write("    red:     '" + red +     "'\n")
                 f.write("    green:   '" + green +   "'\n")
                 f.write("    blue:    '" + blue +    "'\n")
